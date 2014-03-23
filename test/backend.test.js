3abf4a4230de756428637caa619b21bac849b799
jesse keane <me@jessekeane.me>
2014-03-23 15:31:20 -0700

    add tests

--- test/backend.test.js
+++ test/backend.test.js
@@ -1,15 +1,19 @@
-var fs = require('fs')
+var stream = require('stream')
+  , fs = require('fs')
 
 var fs_back = require('../')
   , rmrf = require('rimraf')
   , test = require('tape')
 
-// pre-emptive teardown, just in case.
-//teardown()
+teardown()
 
-var backend = fs_back('./meta', './user', './tgz')
+var backend = fs_back(
+    './test/dirs/meta'
+  , './test/dirs/user'
+  , './test/dirs/tgz'
+)
 
-test('stores meta-data', function(t) {
+test('set_meta stores meta-data', function(t) {
   var test_data = {test: 'data'}
 
   t.plan(3)
@@ -23,13 +27,13 @@ test('stores meta-data', function(t) {
       , JSON.stringify(test_data, null, 2), 'callback gets stringified data'
     )
     t.equal(
-        fs.readFileSync('./meta/dummy.json').toString()
+        fs.readFileSync('./test/dirs/meta/dummy.json').toString()
       , JSON.stringify(test_data, null, 2)
     )
   }
 })
 
-test('gets meta-data', function(t) {
+test('get_meta gets meta-data', function(t) {
   var expected = {test: 'data'}
 
   t.plan(2)
@@ -42,7 +46,7 @@ test('gets meta-data', function(t) {
   }
 })
 
-test('stores user-data', function(t) {
+test('set_user stores user-data', function(t) {
   var test_data = {test: 'data'}
 
   t.plan(3)
@@ -56,13 +60,13 @@ test('stores user-data', function(t) {
       , JSON.stringify(test_data, null, 2), 'callback gets stringified data'
     )
     t.equal(
-        fs.readFileSync('./user/dummy.json').toString()
+        fs.readFileSync('./test/dirs/user/dummy.json').toString()
       , JSON.stringify(test_data, null, 2)
     )
   }
 })
 
-test('gets user-data', function(t) {
+test('get_user gets user-data', function(t) {
   var expected = {test: 'data'}
 
   t.plan(2)
@@ -75,10 +79,49 @@ test('gets user-data', function(t) {
   }
 })
 
-//teardown()
+test('set_tarball creates writable stream to tgz file', function(t) {
+  var dummy_contents = 'drangus'
+
+  var set_tarball = backend.set_tarball('dummy', '1.2.3')
+
+  t.plan(3)
+
+  t.ok(set_tarball.write)
+  t.equal(typeof set_tarball.write, 'function')
+
+  set_tarball.on('finish', function() {
+    t.equal(
+        fs.readFileSync('./test/dirs/tgz/dummy@1.2.3.tgz').toString()
+      , dummy_contents
+    )
+  })
+
+  set_tarball.end(dummy_contents)
+})
+
+test('get_tarball streams tgz contents', function(t) {
+  var expected = 'drangus'
+
+  var get_tarball = backend.get_tarball('dummy', '1.2.3')
+    , data = ''
+
+  t.plan(5)
+
+  t.ok(get_tarball.pipe)
+  t.ok(get_tarball.read)
+  t.equal(typeof get_tarball.pipe, 'function')
+  t.equal(typeof get_tarball.read, 'function')
+
+  get_tarball.on('data', function(chunk) {
+    data += chunk
+  })
+
+  get_tarball.on('end', function() {
+    t.equal(data, expected)
+    teardown()
+  })
+})
 
 function teardown() {
-  rmrf.sync('./meta')
-  rmrf.sync('./user')
-  rmrf.sync('./tgz')
+  rmrf.sync('./test/dirs')
 }
