4afa17fe8b019b2ae0a14b6d43ab29720f82247d
jesse keane <me@jessekeane.me>
2014-04-24 16:07:07 -0700

    add event emitters, clean up code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var path = require('path')
+var EE = require('events').EventEmitter
+  , path = require('path')
   , fs = require('fs')
 
 var jrs = require('json-readdir-stream')
@@ -13,7 +14,8 @@ var CWD = process.cwd()
 module.exports = fs_back
 
 function fs_back(_meta_dir, _user_dir, _tarballs_dir, _store_dir) {
-  var tarballs_dir
+  var backend = new EE
+    , tarballs_dir
     , store_dir
     , user_dir
     , meta_dir
@@ -28,42 +30,84 @@ function fs_back(_meta_dir, _user_dir, _tarballs_dir, _store_dir) {
   mkdirp.sync(user_dir)
   mkdirp.sync(meta_dir)
 
-  return {
-      getUser: get_user
-    , createUserStream: get_users
-    , setUser: set_user
-    , getMeta: get_meta
-    , createMetaStream: get_metas
-    , setMeta: set_meta
-    , getTarball: get_tarball
-    , setTarball: set_tarball
-    , get: get_store
-    , createStream: get_stores
-    , set: set_store
+  backend.getUser = get(user_dir)
+  backend.setUser = set(user_dir, 'setUser')
+  backend.removeUser = remove(user_dir, 'removeUser')
+  backend.createUserStream = stream_all(user_dir)
+  backend.getMeta = get(meta_dir)
+  backend.setMeta = set(meta_dir, 'setMeta')
+  backend.removeMeta = remove(meta_dir, 'removeMeta')
+  backend.createMetaStream = stream_all(meta_dir)
+  backend.get = get(store_dir)
+  backend.set = set(store_dir, 'set')
+  backend.remove = remove(store_dir, 'remove')
+  backend.createStream = stream_all(store_dir)
+  backend.getTarball = get_tarball
+  backend.setTarball = set_tarball
+  backend.removeTarball = remove_tarball
+
+  return backend
+
+  function get(dir) {
+    return function get_data(key, _done) {
+      var done = _done || noop
+
+      read_json(join(dir, key), done)
+    }
   }
 
-  function get_user(name, done) {
-    read_json(join(user_dir, name), done)
+  function stream_all(dir) {
+    return function stream_data(options) {
+      return jrs(dir, options)
+    }
   }
 
-  function get_users(options) {
-    return jrs(user_dir, options)
+  function set(dir, event_name) {
+    return function set_data(key, data, _done) {
+      var done = _done || noop
+        , old_data
+
+      get(dir)(key, got_old)
+
+      function got_old(err, old) {
+        if(err) return done(err)
+
+        old_data = old
+
+        write_json(join(dir, key), data, saved_new)
+      }
+
+      function saved_new(err, data) {
+        if(err) return done(err)
+
+        done(null, data, old_data)
+        backend.emit(event_name, key, data, old_data)
+      }
+    }
   }
 
-  function set_user(name, data, done) {
-    write_json(join(user_dir, name), data, done)
-  }
+  function remove(dir, event_name) {
+    return function remove_data(key, _done) {
+      var done = _done || noop
+        , old_data
 
-  function get_meta(name, done) {
-    read_json(join(meta_dir, name), done)
-  }
+      get(dir)(key, got_old)
 
-  function get_metas(options) {
-    return jrs(meta_dir, options)
-  }
+      function got_old(err, data) {
+        if(err) return done(err)
 
-  function set_meta(name, data, done) {
-    write_json(join(meta_dir, name), data, done)
+        old_data = data
+
+        fs.unlink(join(dir, key + '.json'), removed)
+      }
+
+      function removed(err) {
+        if(err) return done(err)
+
+        done(null, old_data)
+        backend.emit(event_name, key, old_data)
+      }
+    }
   }
 
   function get_tarball(name, version) {
@@ -78,16 +122,8 @@ function fs_back(_meta_dir, _user_dir, _tarballs_dir, _store_dir) {
     )
   }
 
-  function get_store(name, done) {
-    read_json(join(user_dir, name), done)
-  }
-
-  function get_stores(options) {
-    return jrs(store_dir, options)
-  }
-
-  function set_store(name, data, done) {
-    write_json(join(store_dir, name), data, done)
+  function remove_tarball(name, version, callback) {
+    fs.unlink(join(tarballs_dir, name + '@' + version + '.tgz'), callback)
   }
 }
 
@@ -103,7 +139,7 @@ function write_json(filename, data, ready) {
   function write_done(err) {
     if(err) return ready(err)
 
-    ready(null, json_data)
+    ready(null, data)
   }
 }
 
@@ -120,3 +156,5 @@ function read_json(filename, ready) {
     }
   }
 }
+
+function noop() {}
